da84adfa1aaa306470057db12985b639acb0775c
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-04-28 15:25:24 +0530

    Update kiritostrings.py

--- SaitamaRobot/modules/kiritostrings.py
+++ SaitamaRobot/modules/kiritostrings.py
@@ -17,3 +17,12 @@ KAZUTO = (
   "Sung Jin Woo is a demon indeed...",
   "Dont u dare play games with me... I will call the GodlyDemon Sung and @Dojeto ...",
 )
+
+KIRI_IMG = (
+  "https://telegra.ph/file/d5b02e632ef719dc542d0.jpg",
+  "https://telegra.ph/file/77cf975c0328ef9e1a100.jpg",
+  "https://telegra.ph/file/715647eb4930b066dc762.jpg",
+  "https://telegra.ph/file/999c5471ace323e131b89.jpg",
+  "https://telegra.ph/file/349482136faabd67c9f94.jpg",
+  "https://telegra.ph/file/c499269b6d25ec7376d47.jpg",
+)
