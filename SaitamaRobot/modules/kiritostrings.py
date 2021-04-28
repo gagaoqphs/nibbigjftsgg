6f6d2e5e4e5d64ef067951ed3adad7b44adce7a1
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-04-28 22:10:17 +0530

    Update kiritostrings.py

--- SaitamaRobot/modules/kiritostrings.py
+++ SaitamaRobot/modules/kiritostrings.py
@@ -19,10 +19,14 @@ KAZUTO = (
 )
 
 KIRI_IMG = (
-  "https://telegra.ph/file/d5b02e632ef719dc542d0.jpg",
-  "https://telegra.ph/file/77cf975c0328ef9e1a100.jpg",
-  "https://telegra.ph/file/715647eb4930b066dc762.jpg",
-  "https://telegra.ph/file/999c5471ace323e131b89.jpg",
-  "https://telegra.ph/file/349482136faabd67c9f94.jpg",
-  "https://telegra.ph/file/c499269b6d25ec7376d47.jpg",
+  "https://i.imgur.com/SLawXBp.jpeg",
+  "https://i.imgur.com/G8NjbXE.jpg",
+  "https://i.imgur.com/ZpmxarX.png",
+  "https://i.imgur.com/VmmaZNW.jpeg",
+  "https://i.imgur.com/m1lXahZ.jpg",
+  "https://i.imgur.com/kbv3d6k.png",
+  "https://i.imgur.com/kz1eVpu.jpg",
+  "https://i.imgur.com/B5IFeRd.jpeg",
+  "https://i.imgur.com/weFRPTk.jpg",
+  "https://i.imgur.com/AqxPYun.jpeg",
 )
