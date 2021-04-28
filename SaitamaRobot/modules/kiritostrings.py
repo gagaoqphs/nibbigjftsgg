842971ad5c6d0f2cdad39a772731ba39bb6d8f5d
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-04-28 14:34:36 +0530

    Update kiritostrings.py

--- SaitamaRobot/modules/kiritostrings.py
+++ SaitamaRobot/modules/kiritostrings.py
@@ -1,19 +1,19 @@
 KAZUTO = (
-  "I'd rather die with someone than let that person die before my eyes."
-  "Levels are just numbers...."
-  "Well it doesn't matter... says the Sung Jin-Woo"
-  "Dojeto left TG who will help Sung??"
-  "Its useless to ask for... happiness is a lie"
-  "@SirAbraxas is a Simp so better watch out"
-  "Durov has left the chat"
-  "Durov has entered the chat"
-  "Ayanokoji or Sung and the ArinX"
-  "Why run for happiness when u cant have it"
-  "Real pain is the result of losing someone you care about."
-  "You can go anywhere you want in this world with a single blade."
-  "It is pointless to question who someone really is... @Dojeto is a pro indeed"
-  "No Matter What, My Heart Will Be Forever Yours, And I Will Fight Until The Day I See You Again."
-  "I Thought That The Closer The Real And Virtual World Got, The Better The Future Would Be. But The More The Boundary Between Them Blurs, The More It Starts To Trick People."
-  "Sung Jin Woo is a demon indeed..."
-  "Dont u dare play games with me... I will call the GodlyDemon Sung and @Dojeto ..."
+  "I'd rather die with someone than let that person die before my eyes.",
+  "Levels are just numbers....",
+  "Well it doesn't matter... says the Sung Jin-Woo",
+  "Dojeto left TG who will help Sung??",
+  "Its useless to ask for... happiness is a lie",
+  "@SirAbraxas is a Simp so better watch out",
+  "Durov has left the chat",
+  "Durov has entered the chat",
+  "Ayanokoji or Sung and the ArinX",
+  "Why run for happiness when u cant have it",
+  "Real pain is the result of losing someone you care about.",
+  "You can go anywhere you want in this world with a single blade.",
+  "It is pointless to question who someone really is... @Dojeto is a pro indeed",
+  "No Matter What, My Heart Will Be Forever Yours, And I Will Fight Until The Day I See You Again.",
+  "I Thought That The Closer The Real And Virtual World Got, The Better The Future Would Be. But The More The Boundary Between Them Blurs, The More It Starts To Trick People.",
+  "Sung Jin Woo is a demon indeed...",
+  "Dont u dare play games with me... I will call the GodlyDemon Sung and @Dojeto ...",
 )
