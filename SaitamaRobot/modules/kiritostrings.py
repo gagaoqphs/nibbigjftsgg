c77c67cea3b9bdffb4f57e79d49c6c8159bcb5f3
Dojeto <81398258+Dojeto@users.noreply.github.com>
2021-04-28 14:19:42 +0530

    Create kiritostrings.py

---
+++ SaitamaRobot/modules/kiritostrings.py
@@ -0,0 +1,8 @@
+KAZUTO = (
+  "https://telegra.ph/file/d5b02e632ef719dc542d0.jpg"
+  "https://telegra.ph/file/77cf975c0328ef9e1a100.jpg"
+  "https://telegra.ph/file/715647eb4930b066dc762.jpg"
+  "https://telegra.ph/file/999c5471ace323e131b89.jpg"
+  "https://telegra.ph/file/349482136faabd67c9f94.jpg"
+  "https://telegra.ph/file/c499269b6d25ec7376d47.jpg"
+)
