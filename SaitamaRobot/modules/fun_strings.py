d37208212dbac3db7f3f0fd1e0a58415cb2b04f0
Dojeto <81398258+Dojeto@users.noreply.github.com>
2021-04-28 13:09:08 +0530

    Update fun_strings.py

--- SaitamaRobot/modules/fun_strings.py
+++ SaitamaRobot/modules/fun_strings.py
@@ -25,6 +25,15 @@ GIFS = [
     "CgACAgUAAx0CU_rCTAABAjdSX1s3fq5iEJ64YeQLKI8cD7CSuSEAAlUBAAJu09hW5iqWB0hTPD4bBA",
 ]
 
+KIRI_PHOTO = (
+  "https://telegra.ph/file/d5b02e632ef719dc542d0.jpg"
+  "https://telegra.ph/file/77cf975c0328ef9e1a100.jpg"
+  "https://telegra.ph/file/715647eb4930b066dc762.jpg"
+  "https://telegra.ph/file/999c5471ace323e131b89.jpg"
+  "https://telegra.ph/file/349482136faabd67c9f94.jpg"
+  "https://telegra.ph/file/c499269b6d25ec7376d47.jpg"
+)
+
 SLAP_SAITAMA_TEMPLATES = (
     "Slap me one more time and I'll mute you.",
     "Stop slapping me. REEEEEEEEEEEEEE.",
