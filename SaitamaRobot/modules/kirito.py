3e0ee12d231565a688007b4c2da13b7d9d1d8c41
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-04-28 15:23:34 +0530

    Update kirito.py

--- SaitamaRobot/modules/kirito.py
+++ SaitamaRobot/modules/kirito.py
@@ -11,8 +11,18 @@ from telegram.ext import CallbackContext, run_async
 def kazuto(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(kiritostrings.KAZUTO))
    
+@run_async
+def kirito(update: Update, context: CallbackContext):
+    message = update.effective_message
+    name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
+    reply_photo = message.reply_to_message.reply_photo if message.reply_to_message else message.reply_photo
+    reply_photo(
+        random.choice(kiritostrings.KIRI_IMG), caption=f'*To command is to serve, nothing more and nothing less...\nCommand me {name}*')
+
 
 
 KAZUTO_HANDLER = DisableAbleCommandHandler("kazuto", kazuto)
+KIRITO_HANDLER = DisableAbleCommandHandler("kirito", kirito)
 
 dispatcher.add_handler(KAZUTO_HANDLER)
+dispatcher.add_handler(KIRITO_HANDLER)
