a7de49aeedf788f0a9e58b4d240803beedd1e0c5
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-05-04 18:12:09 +0530

    Update kirito.py

--- SaitamaRobot/modules/kirito.py
+++ SaitamaRobot/modules/kirito.py
@@ -1,5 +1,9 @@
 import html
 import random
+
+from telegram.error import BadRequest
+from tswift import Song
+
 import SaitamaRobot.modules.kiritostrings as kiritostrings
 from SaitamaRobot import dispatcher
 from telegram import ParseMode, Update
@@ -23,8 +27,40 @@ def kirito(update: Update, context: CallbackContext):
     reply_photo(
         random.choice(kiritostrings.KIRI_IMG), caption=f'*Be my opponent {name}*')
     
+    
+    @run_async
+@typing_action
+def lyrics(update: Update, context: CallbackContext):
+    bot, args = context.bot, context.args
+    msg = update.effective_message
+    query = " ".join(args)
+    song = ""
+    if not query:
+        msg.reply_text("You haven't specified which song to look for!")
+        return
+    song = Song.find_song(query)
+    if song:
+        if song.lyrics:
+            reply = song.format()
+        else:
+            reply = "Couldn't find any lyrics for that song!"
+    else:
+        reply = "Song not found!"
+    if len(reply) > 4090:
+        with open("lyrics.txt", 'w') as f:
+            f.write(f"{reply}\n\n\nOwO UwU OmO")
+        with open("lyrics.txt", 'rb') as f:
+            msg.reply_document(document=f,
+            caption="Message length exceeded max limit! Sending as a text file.")
+    else:
+        msg.reply_text(reply)
+
+
+    
 KIRITO_HANDLER = DisableAbleCommandHandler("kirito", kirito)
-    KAZUTO_HANDLER = DisableAbleCommandHandler("kazuto", kazuto)
+KAZUTO_HANDLER = DisableAbleCommandHandler("kazuto", kazuto)
+LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics)
 
 dispatcher.add_handler(KAZUTO_HANDLER)
 dispatcher.add_handler(KIRITO_HANDLER)
+dispatcher.add_handler(LYRICS_HANDLER)
