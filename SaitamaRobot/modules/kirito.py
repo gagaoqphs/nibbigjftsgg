91581177fadde35a21813ad9b2320dc3b90d448d
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-05-06 15:56:19 +0530

    Update kirito.py

--- SaitamaRobot/modules/kirito.py
+++ SaitamaRobot/modules/kirito.py
@@ -4,6 +4,9 @@ import random
 from telegram.error import BadRequest
 from tswift import Song
 
+import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
+
 import SaitamaRobot.modules.kiritostrings as kiritostrings
 from SaitamaRobot import dispatcher
 from telegram import ParseMode, Update
@@ -13,6 +16,44 @@ from telegram.ext import CallbackContext, run_async
 from SaitamaRobot.modules.helper_funcs.chat_status import (is_user_admin)
 from SaitamaRobot.modules.helper_funcs.extraction import extract_user
 from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+
+@run_async
+def wiki(update: Update, context: CallbackContext):
+    msg = update.effective_message.reply_to_message if update.effective_message.reply_to_message else update.effective_message
+    res = ""
+    if msg == update.effective_message:
+        search = msg.text.split(" ", maxsplit=1)[1]
+    else:
+        search = msg.text
+    try:
+        res = wikipedia.summary(search)
+    except DisambiguationError as e:
+        update.message.reply_text(
+            "Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>"
+            .format(e),
+            parse_mode=ParseMode.HTML)
+    except PageError as e:
+        update.message.reply_text(
+            "<code>{}</code>".format(e), parse_mode=ParseMode.HTML)
+    if res:
+        result = f"<b>{search}</b>\n\n"
+        result += f"<i>{res}</i>\n"
+        result += f"""<a href="https://en.wikipedia.org/wiki/{search.replace(" ", "%20")}">Read more...</a>"""
+        if len(result) > 4000:
+            with open("result.txt", 'w') as f:
+                f.write(f"{result}\n\nUwU OwO OmO UmU")
+            with open("result.txt", 'rb') as f:
+                context.bot.send_document(
+                    document=f,
+                    filename=f.name,
+                    reply_to_message_id=update.message.message_id,
+                    chat_id=update.effective_chat.id,
+                    parse_mode=ParseMode.HTML)
+        else:
+            update.message.reply_text(
+                result,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True)
 
 
 @run_async
@@ -55,12 +96,27 @@ def lyrics(update: Update, context: CallbackContext):
     else:
         msg.reply_text(reply)
 
+def check_message(context: CallbackContext, message):
+    reply_msg = message.reply_to_message
+
+    if message.text.lower() == "kazuya":
+        return True
+    if reply_msg:
+        if reply_msg.from_user.id == context.bot.get_me().id:
+            return True
+    else:
+        return False
+    
+    
 
     
 KIRITO_HANDLER = DisableAbleCommandHandler("kirito", kirito)
 KAZUTO_HANDLER = DisableAbleCommandHandler("kazuto", kazuto)
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics)
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
 
+
+dispatcher.add_handler(WIKI_HANDLER)
 dispatcher.add_handler(KAZUTO_HANDLER)
 dispatcher.add_handler(KIRITO_HANDLER)
 dispatcher.add_handler(LYRICS_HANDLER)
