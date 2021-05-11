1fbef65889d18d6e96bedc841b75b0b30f914d52
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-05-11 19:34:17 +0530

    Update AIplus.py

--- SaitamaRobot/modules/AIplus.py
+++ SaitamaRobot/modules/AIplus.py
@@ -48,3 +48,69 @@ async def hmm(event):
            await event.reply(result)
     except CFError as e:
            print(e)
+        
+        
+        
+@register(pattern="Waifu (.*)")
+async def waifu(animu):
+    xx = await eor(animu, "`Processing...`")
+    # """Creates random anime sticker!"""
+    text = animu.pattern_match.group(1)
+    if not text:
+        if animu.is_reply:
+            text = (await animu.get_reply_message()).message
+        else:
+            await xx.edit("`You haven't written any article, Waifu is going away.`")
+            return
+    waifus = [32, 33, 37, 40, 41, 42, 58, 20]
+    finalcall = "#" + (str(random.choice(waifus)))
+    try:
+        sticcers = await ultroid_bot.inline_query(
+            "stickerizerbot",
+            f"{finalcall}{(deEmojify(text))}",
+        )
+        await sticcers[0].click(
+            animu.chat_id,
+            reply_to=animu.reply_to_msg_id,
+            silent=True if animu.is_reply else False,
+            hide_via=True,
+        )
+        await xx.delete()
+    except ChatSendInlineForbiddenError:
+        await xx.edit("`Boss ! I cant use inline things here...`")
+    except ChatSendStickersForbiddenError:
+        await xx.edit("Sorry boss, I can't send Sticker Here !!")
+
+@register(pattern="convert (.*)")
+async def uconverter(event):
+    xx = await eor(event, "`Processing...`")
+    a = await event.get_reply_message()
+    ok = ["image/webp", "application/x-tgsticker"]
+    if not (a.media and a.media.document and a.media.document.mime_type in ok):
+        return await eor(event, "`Reply to a Sticker...`")
+    input = event.pattern_match.group(1)
+    b = await event.client.download_media(a, "resources/downloads/")
+    if "gif" in input:
+        cmd = ["lottie_convert.py", b, "something.gif"]
+        file = "something.gif"
+    elif "img" in input:
+        cmd = ["lottie_convert.py", b, "something.png"]
+        file = "something.png"
+    elif "sticker" in input:
+        cmd = ["lottie_convert.py", b, "something.webp"]
+        file = "something.webp"
+    else:
+        return await xx.edit("**Please select from gif/img/sticker**")
+    process = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    stderr.decode().strip()
+    stdout.decode().strip()
+    os.remove(b)
+    await event.client.send_file(event.chat_id, file, force_document=False)
+    os.remove(file)
+    await xx.delete()
+
