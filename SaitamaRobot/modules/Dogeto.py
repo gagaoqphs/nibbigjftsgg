ae809f0091df06f207c837e8168e5e54d1a77054
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-05-15 13:20:31 +0530

    Create Dogeto.py

---
+++ SaitamaRobot/modules/Dogeto.py
@@ -0,0 +1,41 @@
+import requests as r
+from pyrogram.types import Message
+from pyrogram import filters
+from SaitamaRobot import app
+from SaitamaRobot.utils import cust_filter
+import urllib.request
+import json
+import asyncio
+from random import randint
+from pyrogram import filters
+from pyrogram.types import Message
+
+
+@app.on_message(cust_filter.command(commands=("cat")) & ~filters.edited)
+async def cat(_, message: Message):
+    with urllib.request.urlopen(
+        "https://api.thecatapi.com/v1/images/search"
+    ) as url:
+        data = json.loads(url.read().decode())
+    cat_url = (data[0]['url'])
+    await message.reply_photo(cat_url)
+
+
+@app.on_message(cust_filter.command(commands=("reddit")) & ~filters.edited)
+async def reddit(_, message: Message):
+    app.set_parse_mode("html")
+    if len(message.command) != 2:
+        await message.reply_text("/reddit needs an argument")
+    subreddit = message.command[1]
+    res = r.get(f"https://meme-api.herokuapp.com/gimme/{subreddit}")
+    res = res.json()
+
+    rpage = res.get(str("subreddit"))  # Subreddit
+    title = res.get(str("title"))  # Post title
+    memeu = res.get(str("url"))  # meme pic url
+    plink = res.get(str("postLink"))
+
+    caps = f"<b>Title</b>: {title}\n"
+    caps += f"<b>Subreddit: </b>r/{rpage}\n"
+    caps += f"<b>PostLink:</b> {plink}"
+    await message.reply_photo(photo=memeu, caption=(caps))
